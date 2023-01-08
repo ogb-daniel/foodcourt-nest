b113c910eaf97ad53355f4688c5995dfa83e151e
ogb-daniel <ogbuikedaniel@gmail.com>
2023-01-08 22:25:47 +0100

    added home route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
 import { BrandModule } from './brand/brand.module';
 
 @Module({
   imports: [BrandModule],
+  controllers: [AppController],
 })
 export class AppModule {}
